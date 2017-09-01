9d410568a10eb277186817937c8000842c30400f
Chris Boette <cboette@gmail.com>
2017-09-01 11:17:27 -0500

    Add client code. Make first image in returned urls big and on the left. Random four go on the right.

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import shuffle from 'lodash.shuffle';
 
-const Grid = ({ urls }) => (
-  <div className='images'>
-    {urls.map((image, index) => {
-      return (
-        <div className='image' key={index}>
-          <img src={image} alt='glitched portrait' />
-        </div>
-      );
-    })}
-  </div>
-);
+const Grid = ({ urls }) => {
+  const shuffledFour = shuffle(urls.slice(1)).slice(0, 4);
+  return <div className='images'>
+    <div className='newest'>
+      <img src={urls[0]} alt='newest glitched portrait' />
+    </div>
+    <div className='grid'>
+      {shuffledFour.map((image, index) => {
+        return (
+          <div className='image' key={index}>
+            <img src={image} alt='rando glitched portraits' />
+          </div>
+        );
+      })}
+    </div>
+  </div>;
+};
 
 export default Grid;
