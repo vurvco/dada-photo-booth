5926015c20a321ddc75472a88bf823d6900e40e2
Chris Boette <cboette@gmail.com>
2018-02-17 18:48:14 -0600

    WIP

--- camera/src/App.js
+++ camera/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import AlertContainer from 'react-alert';
+import AlertContainer from 'react-alert';
 
 import './App.css';
 
@@ -18,99 +18,111 @@ const postOptions = src => ({
   }
 });
 
-// const alertOptions = {
-//   offset: 14,
-//   position: 'top right',
-//   theme: 'dark',
-//   time: 5000,
-//   transition: 'scale'
-// };
+const alertOptions = {
+  offset: 14,
+  position: 'top right',
+  theme: 'dark',
+  time: 5000,
+  transition: 'scale'
+};
 
-const Processing = () => (<div><p>Adam\'s cool shit!</p></div>);
-
-// const Camera = ({ setRef, capture }) => (
-//   <div className='camera'>
-//     <Webcam
-//       audio={false}
-//       height={window.innerHeight}
-//       ref={setRef}
-//       screenshotFormat='image/jpeg'
-//       width={window.innerWidth}
-//     />
-//     <button onClick={capture}>GIF it</button>
-//   </div>
-// );
+// const Processing = () => (<div><p>cool shit from adam!</p></div>);
 
 export default class App extends Component {
   constructor (props) {
     super(props);
-    this.state = {isGenerating: false};
+    this.state = { isGenerating: false };
     this.tracker = null;
+    this.capture = this.capture.bind(this);
+    this.captureEvent = this.captureEvent.bind(this);
     this.captureOnKeyDown = this.captureOnKeyDown.bind(this);
+  }
+
+  showAlert (text, type) {
+    this.msg.show(text, {
+      type,
+      time: 3000
+    });
   }
 
   captureOnKeyDown (event) {
     if (event.keyCode.toString() === SPACEBAR_KEYCODE) {
-      this.capture();
+      // this.capture(event);
     }
   }
 
-  capture () {
-    console.log('triggered capture');
-    const imageSrc = this.webcam.getScreenshot();
-    console.log('imageSrc', imageSrc);
-    // window.fetch(postUrl, postOptions(imageSrc))
-    //   .then(res => {
-    //     console.log({ res });
-    //     if (res.ok) {
-    //       this.showAlert('Photo Uploaded', 'success');
-    //       this.setState({ isGenerating: true });
-    //     } else {
-    //       this.showAlert(`Upload Error: ${res.statusText}`, 'error');
-    //     }
-    //   })
-    //   .catch(err => {
-    //     console.error({ err });
-    //     this.showAlert(`Upload Error: ${err.message}`, 'error');
-    //   });
+  captureEvent (event) {
+    this.capture(event);
   }
 
-  componentDidMount () {
-    document.addEventListener('keydown', this.captureOnKeyDown, false);
+  // capture (event) {
+  //   /*
+  //     navigator.getUserMedia({video: true}, function(stream) {
+  //       video.src = window.URL.createObjectURL(stream);
+  //       localMediaStream = stream;
+  //     }, function(error){console.error(error)});
+  //   */
+  //   window.navigator.getUserMedia({video: true}, (stream) => {
+  //     document.querySelector('video').src = window.URL.createObjectURL(stream);
+  //     document.querySelector('a').href = this.refs.canvas.toDataURL('image/png');
+  //     window.fetch(postUrl, postOptions(this.refs.canvas.toDataURL('image/png')))
+  //       .then(res => {
+  //         if (res.ok) {
+  //           this.showAlert('Photo Uploaded', 'success');
+  //           this.setState({ isGenerating: true });
+  //         } else {
+  //           this.showAlert(`Upload Error: ${res.statusText}`, 'error');
+  //         }
+  //       })
+  //       .catch(err => {
+  //         console.error({ err });
+  //         this.showAlert(`Upload Error: ${err.message}`, 'error');
+  //       });
+  //   }, (error) => { console.error(`Err in gum cb: ${error}`); });
+  // }
 
-    this.tracker = new window.tracking.ObjectTracker('face');
-    this.tracker.setInitialScale(4);
-    this.tracker.setStepSize(2);
-    this.tracker.setEdgesDensity(0.1);
+  // componentDidMount () {
+  //   // Capture image with spacebar
+  //   document.addEventListener('keydown', this.captureOnKeyDown, false);
 
-    window.tracking.track(this.refs.cameraOutput, this.tracker, { camera: true });
-    this.tracker.on('track', event => {
-      let context = this.refs.canvas.getContext('2d');
-      context.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+  //   this.tracker = new window.tracking.ObjectTracker('face');
+  //   this.tracker.setInitialScale(4);
+  //   this.tracker.setStepSize(2);
+  //   this.tracker.setEdgesDensity(0.1);
 
-      event.data.forEach(function (rect) {
-        rect.y *= 0.9;
-        rect.height *= 1.33;
+  //   window.tracking.track(this.refs.cameraOutput, this.tracker, { camera: true });
+  //   this.tracker.on('track', (event) => {
+  //     // Stop app from erroring when `<canvas>` gets hidden
+  //     if (this.refs.canvas) {
+  //       let context = this.refs.canvas.getContext('2d');
+  //       context.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
 
-        context.strokeStyle = '#a64ceb';
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.lineWidth = 5;
-        context.font = '11px Helvetica';
-        context.fillStyle = '#fff';
-        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-      });
-    });
-  }
+  //       event.data.forEach((rect) => {
+  //         let { x, y, width, height } = rect;
+  //         y *= 0.9;
+  //         height *= 1.33;
 
-  componentWillUnmount () {
-    this.tracker.removeAllListeners();
-    document.addEventListener('keydown', this.captureOnKeyDown, false);
-  }
+  //         context.strokeStyle = '#a64ceb';
+  //         context.strokeRect(x, y, width, height);
+  //         context.lineWidth = 5;
+  //         context.font = '11px Helvetica';
+  //         context.fillStyle = '#fff';
+  //         context.fillText(`x: ${x}px`, x + width + 5, y + 11);
+  //         context.fillText(`y: ${y}px`, x + width + 5, y + 22);
+  //       });
+  //     }
+  //   });
+  // }
+
+  // componentWillUnmount () {
+  //   this.tracker.removeAllListeners();
+  //   document.addEventListener('keydown', this.captureOnKeyDown, false);
+  // }
 
   render () {
     return (
       <div className='container'>
+        {/* <AlertContainer ref={a => { this.msg = a; }} {...alertOptions} />
         {
           this.state.isGenerating
             ? <Processing />
@@ -119,59 +131,10 @@ export default class App extends Component {
                 <video ref='cameraOutput' width='640' height='480' preload autoPlay loop muted />
                 <canvas ref='canvas' width='640' height='480' />
               </div>
-              <button onClick={this.capture}>GIF it</button>
+              <a className='button' href='' download='snapshot.png' onClick={this.captureEvent}>GIF it</a>
             </div>)
-        }
+        } */}
       </div>
     );
   }
 }
-
-// class App extends Component {
-//   constructor (props) {
-//     super(props);
-//     this.state = {isGenerating: false};
-
-//     this.setRef = (webcam) => {
-//       this.webcam = webcam;
-//     };
-
-//     this.showAlert = (text, type) => {
-//       this.msg.show(text, {
-//         type,
-//         time: 3000
-//       });
-//     };
-
-//     this.capture = () => {
-//       const imageSrc = this.webcam.getScreenshot();
-//       window.fetch(postUrl, postOptions(imageSrc))
-//         .then(res => {
-//           console.log({ res });
-//           if (res.ok) {
-//             this.showAlert('Photo Uploaded', 'success');
-//             this.setState({ isGenerating: true });
-//           } else {
-//             this.showAlert(`Upload Error: ${res.statusText}`, 'error');
-//           }
-//         })
-//         .catch(err => {
-//           console.error({ err });
-//           this.showAlert(`Upload Error: ${err.message}`, 'error');
-//         });
-//     };
-//   }
-
-//   render () {
-//     return (
-//       <div className='App'>
-//         <AlertContainer ref={a => { this.msg = a; }} {...alertOptions} />
-//         {this.state.isGenerating
-//           ? <Processing />
-//           : <Camera setRef={this.setRef} capture={this.capture} />}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
