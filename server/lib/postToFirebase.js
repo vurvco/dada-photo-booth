ba5be48d8d5b6f1a551d88ccbeecb8f99b6630e7
Chris Boette <cboette@gmail.com>
2018-02-24 11:04:37 -0600

    [9] Post gif to Firebase
    [9] Upload file to bucket.
    
    [9] Post to Firebase storage and database

--- server/lib/postToFirebase.js
+++ server/lib/postToFirebase.js
@@ -2,55 +2,53 @@
 
 require('dotenv').config();
 
+const admin = require('firebase-admin');
 const util = require('util');
 const uuid = require('uuid');
+
 const exec = util.promisify(require('child_process').exec);
-// This import loads the firebase namespace along with all its type information.
-const firebase = require('firebase/app');
 
-// These imports load individual services into the firebase namespace.
-require('firebase/database');
-require('firebase/storage');
+const serviceAccount = require('../firebase-admin-key.json');
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+// Init
+const BUCKET_NAME = '2018_hello_world';
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  projectId: process.env.FIREBASE_PROJECT_ID
+});
+
+// Utils
+const newName = `${uuid.v4()}.gif`;
+const saveLocallyAs = (name) => `cp glitch.gif ${name}`;
+
+// File Upload
+const storage = app.storage();
+const bucket = storage.bucket();
+
+const uploadFile = (name) => {
+  const options = { destination: `${BUCKET_NAME}/${name}.gif` };
+  console.log(`options in uploadFile: ${JSON.stringify(options)}`);
+  return bucket.upload(`glitch.gif`, options);
 };
-firebase.initializeApp(config);
 
-// For the Database
-const filenameRef = firebase.database().ref('fall-event-2017/filenames');
-
-// For File Storage
-const storage = firebase.storage();
-const storageRef = storage.ref();
-const bucket = storageRef.child('fall-event-2017');
-
-const move = (name) => `mv glitch.gif ${name}`;
-
-const saveName = (name) => {
+// Save Filenames
+const filenameRef = app.database().ref('2018-hello-world/filenames');
+const saveFilenameToDatabase = (name) => {
+  console.log(`saveFilenameToDatabase: ${name}`);
   return filenameRef.push(name);
 };
 
-const uploadFile = (name) => {
-  const imageRef = bucket.child(name);
-  return util.promisify(imageRef.put(name, metadata));
-};
-
 const main = () => {
-  const newName = `${uuid.v4()}.gif`;
-  // todo: save glitch.gif as `${uuid.v4()}.gif`
-  exec(move(newName))
-    .then(() => saveName(newName))
+  return exec(saveLocallyAs(newName))
     .then(() => uploadFile(newName))
-    .then((res) => console.log(`Glitch image added to FB: ${res}`))
-    .catch((err) => err);
+    .then(() => saveFilenameToDatabase(newName))
+    .then((res) => console.log(`Glitch image added to Firebase: ${res}`))
+    .catch((err) => {
+      console.log(`Err: ${JSON.stringify(err)}`);
+      throw err;
+    });
 };
 
-// todo: append filename to firebase database array at
-//       dada-photo-booth/fall-event-2017/filenames
-
-// and then...
-// https://github.com/desmondmorris/node-twitter/tree/master/examples#media
+module.exports.main = main;
